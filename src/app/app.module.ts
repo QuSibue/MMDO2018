3af2fbaa5ff5c1bb44842a310fe17efd44e72494
Quentin Sibué <quentin.sibue@live.fr>
2018-03-28 15:55:30 +0200

    Début TP2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DetailsPage } from '../pages/details/details';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
